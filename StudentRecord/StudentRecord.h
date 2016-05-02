233d852b788dce5ec830eb1928d4e6c811e61d14
Stephen Carter <stephen.carter@mail.mcgill.ca>
2016-05-02 18:02:54 -0400

    added comments to headers, need to finish cleaning them up

--- StudentRecord/StudentRecord.h
+++ StudentRecord/StudentRecord.h
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef __STUDENTRECORD
+#define __STUDENTRECORD
 #include <string>
 #include <algorithm>
 #include <vector>
@@ -8,25 +10,30 @@ using namespace std;
 class StudentRecord
 {
 public:
+	//constructors
 	StudentRecord(string name, int ID, vector<string> classes);
 	StudentRecord(string name, int ID, string classes);
 	StudentRecord(string name, int ID);
 	StudentRecord(string name);
 	StudentRecord(int ID);
 	StudentRecord();
+	//deconstructor
 	~StudentRecord();
+	//get and set names
 	string getName();
 	void setName(string name);
-
+	//methods for modifying classes
 	vector<string> getClasses();
 	void addClass(string classIn);
 	void removeClass(string classIn);
 	void setClasses(vector<string> classes);
 	bool containsClass(string classIn);
+	//check if classmate shares a class
 	bool inClassWith(StudentRecord classmate);
 	string StudentRecord::sharedClass(StudentRecord classmate);
 	vector<string> StudentRecord::sharedClasses(StudentRecord classmate);
-
+	//get and set ID
+	//NOTE: may need to move to protected to work with database
 	int getID();
 	void setID(int ID);
 private:
@@ -34,3 +41,5 @@ private:
 	int ID;
 	vector<string> classes;
 };
+
+#endif
--- StudentRecord/StudentRecordDatabase.h
+++ StudentRecord/StudentRecordDatabase.h
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef __DATABASE
+#define __DATABASE
 #include <unordered_map>
 #include <iostream>
 #include "StudentRecord.h"
@@ -8,17 +10,20 @@ using namespace std;
 class StudentRecordDatabase {
 
 public:
+	//constructors
 	StudentRecordDatabase(vector<StudentRecord> &entries);
 	StudentRecordDatabase(StudentRecord &entry);
 	StudentRecordDatabase();
+	//deconstructors
 	~StudentRecordDatabase();
+	//add and remove students
 	bool addStudent(StudentRecord &entry);
 	bool removeStudent(StudentRecord entry);
-
+	//retrieve student data
 	StudentRecord &getStudentRecord(int ID);
-
+	//print database
 	void printDatabase();
-
+	//for updating classes for student, returns pointer to record
 	StudentRecord* updateClasses(int ID, vector<string> classes);
 	StudentRecord* updateClasses(int ID, string classIn);
 	StudentRecord* removeClass(int ID, string classIn);
@@ -26,3 +31,5 @@ private:
 	int numStudents;
 	unordered_map<int, StudentRecord> hashTable;
 };
+
+#endif
